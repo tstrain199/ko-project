0d59ea0c8eda63f42586583f421a263eb9716a8f
Todd Strain <tstrain199@gmail.com>
2017-12-12 17:23:56 -0500

    Filter not working 1

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -1,6 +1,8 @@
 function ViewModel() {
     var self = this;
 
+    var noop = 1;
+
     self.clubs = model.usualClubs;
 
     self.clubList = ko.observableArray([]);
@@ -21,12 +23,14 @@ function ViewModel() {
 
     self.filterClub = function(club, index){
         var patt = new RegExp(self.filterString());
-        //console.log(club.name + "  " + self.filterString());
+        console.log(club.name + "  " + self.filterString());
         if (patt.test(club.name)){
+          noop++;
+        } else {
           self.removeClub(club);
         //  hideMarker(index);
-        resetMarkers();
         }
+        resetMarkers();
     };
 
     self.filterList = function(){
