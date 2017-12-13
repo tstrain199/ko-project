6319ae57545df19aaad2e530d9010068e30f2c68
tstrain199 <tstrain199@gmail.com>
2017-12-12 23:22:37 -0500

    Working filter

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -7,6 +7,8 @@ function ViewModel() {
 
     self.clubList = ko.observableArray([]);
 
+    self.tmpList = [];
+
     self.filterString = ko.observable('');
 
     self.showClubs = function(category) {
@@ -18,23 +20,22 @@ function ViewModel() {
 
     self.removeClub = function(club) {
         self.clubList.remove(club);
+        console.log("removed" + club);
         //initMap();
+
     };
 
     self.filterClub = function(club, index){
         var patt = new RegExp(self.filterString());
-        console.log(club.name + "  " + self.filterString());
         if (patt.test(club.name)){
-          noop++;
-        } else {
-          self.removeClub(club);
-        //  hideMarker(index);
-        }
-        resetMarkers();
+          self.tmpList.push(club);
+        };
     };
 
     self.filterList = function(){
       self.clubList().forEach(self.filterClub);
+      self.clubList(self.tmpList);
+      resetMarkers();
     };
 
 }
