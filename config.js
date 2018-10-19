c4db68435318084b09a56134a2cfc95fc6048173
tstrain199 <tstrain199@gmail.com>
2018-10-19 15:59:30 -0400

    Added Hamburger Menu

--- config.js
+++ config.js
@@ -4,3 +4,15 @@ var config = {
   FourSquareID : 'XN55DS4DVJZQLSGGSZ3ZWM5HJYLDXMOD21LYJFU2R1DZWQWE',
   FourSquareSecret : 'MNMNXPO1W2BF5LNSWYIUJ0YAHXVSRHDI5SUSWHO0IAKDGXZY'
 }
+ https://api.foursquare.com/v2/venues/search?limit-1&client_id=XN55DS4DVJZQLSGGSZ3ZWM5HJYLDXMOD21LYJFU2R1DZWQWE&client_secret=MNMNXPO1W2BF5LNSWYIUJ0YAHXVSRHDI5SUSWHO0IAKDGXZY&ll=function(){return%20a},function(){return%20b}&query=The%20Blue%20Note&v=20170801
+
+
+ function addMarkerWithTimeout(position, timeout) {
+        window.setTimeout(function() {
+          markers.push(new google.maps.Marker({
+            position: position,
+            map: map,
+            animation: google.maps.Animation.DROP
+          }));
+        }, timeout);
+      }
